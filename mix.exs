e1f2086c4455f8a4f0a3af0aa63c9ed541a3f16a
DJ Daugherty <ddaugher@gmail.com>
2021-04-14 18:11:18 -0400

    updating version in order to push to hex.pm

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule PostgrexPubsubMultiTenant.MixProject do
       app: :postgrex_pubsub_multi_tenant,
       name: "Postgrex PubSub Multitenant",
       description: "A helper for creating and listening to pubsub events from postgres",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
